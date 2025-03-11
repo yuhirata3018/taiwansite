7d6b125c2530feac7226503f8ac0812d3fd414c0
yuhirata3018 <rocksteady3018@gmail.com>
2025-03-11 11:50:19 +0900

    Add files via upload

---
+++ taiwan-site/top/js/top.js
@@ -0,0 +1,32 @@
+'use strict';
+
+img1.onclick = function () {
+  document.getElementById('photo').src = 'top/image/top_gallery_img1.jpg';
+  document.getElementById('photo2').src = 'top/image/top_gallery_img2.jpg';
+  document.getElementById('photo3').src = 'top/image/top_gallery_img3.jpg';
+}
+
+img2.onclick = function () {
+  document.getElementById('photo').src = 'top/image/top_gallery_img2.jpg';
+  document.getElementById('photo2').src = 'top/image/top_gallery_img3.jpg';
+  document.getElementById('photo3').src = 'top/image/top_gallery_img4.jpg';
+}
+
+img3.onclick = function () {
+  document.getElementById('photo').src = 'top/image/top_gallery_img3.jpg';
+  document.getElementById('photo2').src = 'top/image/top_gallery_img4.jpg';
+  document.getElementById('photo3').src = 'top/image/top_gallery_img5.jpg';
+}
+
+img4.onclick = function () {
+  document.getElementById('photo').src = 'top/image/top_gallery_img4.jpg';
+  document.getElementById('photo2').src = 'top/image/top_gallery_img5.jpg';
+  document.getElementById('photo3').src = 'top/image/top_gallery_img1.jpg';
+}
+
+img5.onclick = function () {
+  document.getElementById('photo').src = 'top/image/top_gallery_img5.jpg';
+  document.getElementById('photo2').src = 'top/image/top_gallery_img1.jpg';
+  document.getElementById('photo3').src = 'top/image/top_gallery_img2.jpg';
+}
+
